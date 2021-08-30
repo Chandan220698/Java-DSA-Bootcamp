f77284b3f91a7dc135741f6e8d1c60ea07fdcddf
Chandan Kumar <ck220698@gmail.com>
2021-08-30 12:11:13 +0530

    Update 20. Determine Whether Matrix Can Be Obtained By Rotation.java

--- LeetCode/1. Arrays/20. Determine Whether Matrix Can Be Obtained By Rotation.java
+++ LeetCode/1. Arrays/20. Determine Whether Matrix Can Be Obtained By Rotation.java
@@ -1,1 +1,55 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        
+        boolean flag = true;
+        
+        // first rotation check
+        for(int i = 0;  i < mat.length; i++){
+            for(int j = 0;  j < mat.length; j++){
 
+                if(mat[mat.length-1-j][i] != target[i][j]){
+                    flag = false;
+                }
+            }
+        }
+        // 2nd rotation check
+        if(flag == false){
+            flag = true;
+            for(int i = 0;  i < mat.length; i++){
+                for(int j = 0;  j < mat.length; j++){
+
+                    if(mat[mat.length-1-i][mat.length-1-j] != target[i][j]){
+                        flag = false;
+                    }
+                }
+            }
+        }
+        // 3rd rotation check
+        if(flag == false){
+            flag = true;
+            for(int i = 0;  i < mat.length; i++){
+                for(int j = 0;  j < mat.length; j++){
+
+                    if(mat[j][mat.length-1-i] != target[i][j]){
+                        flag = false;
+                    }
+                }
+            }
+        }
+        // zero rotation
+        if(flag == false){
+            flag = true;
+            for(int i = 0;  i < mat.length; i++){
+                for(int j = 0;  j < mat.length; j++){
+
+                    if(mat[i][j] != target[i][j]){
+                        flag = false;
+                    }
+                }
+            }
+        }
+        
+        return flag;
+    }    
+    
+}
