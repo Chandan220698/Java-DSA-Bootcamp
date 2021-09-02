9f9a9da9422d2d6492673c65a47719a3cc488797
Chandan Kumar <ck220698@gmail.com>
2021-09-02 22:56:23 +0530

    Create Q6b. Peak Index in Mountain Array.java

---
+++ LeetCode/BinarySearch/Q6b. Peak Index in Mountain Array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        
+        int i = 0;
+        for(i = 0;  i < arr.length-1; i++){
+            if(arr[i] > arr[i+1]){
+                break;
+            }
+        }
+        return i;
+        
+    }
+}
